965ea5547309fd0a59eef2a3cb6d11a06cbc52cd
Sophia Baker <72014836+sophiakibaker@users.noreply.github.com>
2024-01-24 15:24:12 +0000

    Update python-snippets.py

--- CheatSheets/python-snippets.py
+++ CheatSheets/python-snippets.py
@@ -4,9 +4,20 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime as dt
 import scipy as scp
+import sys
+import os
+import time
+
+# time script execution
+starttime = time.time()
+# insert code here
+print("This code took",time.time()-starttime,"seconds to run."
 
 # reading files to pandas dataframes
+csv_file = pd.read_csv("filename.csv",delimiter=None, header='infer', names=[name,of,each,column], index_col=None, usecols=None, skiprows=None, skipfooter=0, nrows=None, skip_blank_lines=True)
+xlsx_file = pd.read_excel("filename.xlsx",sheet_name=0, header='infer', names=[name,of,each,column] index_col=None, usecols=None, skiprows=None, skipfooter=0, nrows=None)
 
 # reading text files to arrays
 
