c76af10540ad498e56f3efd3aed2e54ca2d5501e
Sophia Baker <72014836+sophiakibaker@users.noreply.github.com>
2024-01-24 11:20:17 +0000

    Create snippets.py

---
+++ snippets.py
@@ -0,0 +1,17 @@
+# to be filled with snippets I find myself using time and time again
+
+# frequently imported modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy as scp
+
+# reading files to pandas dataframes
+
+# reading text files to arrays
+
+# basic plots
+
+# plots using gridspec
+
+# fitting using scipy
