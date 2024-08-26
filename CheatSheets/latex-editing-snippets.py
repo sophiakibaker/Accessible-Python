614c74e8a59c2e1a8a5e626b3fffd262d6d7b39d
Sophia Baker <72014836+sophiakibaker@users.noreply.github.com>
2024-08-26 17:02:28 +0100

    Update latex-editing-snippets.py

--- CheatSheets/latex-editing-snippets.py
+++ CheatSheets/latex-editing-snippets.py
@@ -2,14 +2,18 @@ import numpy as np
 
 def removeabstracts(bibfile, printabstract=False):
   """This function rewrites bibfile entries to remove the abstract line for a more streamlined set of references. It can be altered for other parts of an entry e.g. to remove dates."""
-  refs = open('bibfile','r')
+  refs = open(bibfile,'r')
   lines = refs.readlines()
-  print(lines)
-  
-  refs = open(bibfile,'w')
+  # print(lines)
+
+  new_lines = []
   for line in lines:
-      if "abstract" not in line:
-          refs.write(line)
-      else:
-        if printabstract==True:  
-          print(line)
+    if "abstract" not in line.strip():
+        new_lines.append(line)
+    else:
+      if printabstract==True:  
+        print(line)
+
+  file = open(bibfile, "w")
+  file.writelines(new_lines)
+  file.close()
